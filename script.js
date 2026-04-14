3015dd5b09129216d964690e2462ad741b5d1096
Gabriel Melo <gabrielmelolopes.br@gmail.com>
2026-04-13 21:57:00 -0300

    Implement item completion feature
    Add functionality to change list item color to green on completion.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lista = document.getElementById('minhaLista');
 const botao = document.getElementById('addBtn');
 const botaoremove = document.getElementById('rmvBtn');
+const concluirBTN = document.getElementById('concluirBTN');
 const input = document.getElementById('input');
 botao.addEventListener("click", () => {
     const novoItem = document.createElement('li');
@@ -14,4 +15,13 @@ botaoremove.addEventListener("click", () => {
     const RemoverItemLista = lista.children[0];
     lista.removeChild(RemoverItemLista);
 });
+
+concluirBTN.addEventListener('click', function () {
+    const itens = document.querySelectorAll('#minhaLista li');
+    itens.forEach(function (li) {
+        li.style.color = 'green';
+    });
+});
+
+
 
